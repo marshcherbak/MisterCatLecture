a1bab352f0bae072bc0f1fc0e52adcf3940c9647
marshcherbak <maryshcherbak@gmail.com>
2017-10-04 13:13:31 +0300

    big cycle

--- Project1/Main.cpp
+++ Project1/Main.cpp
@@ -1,13 +1,50 @@
-#include"Pizza.h"
+﻿#include"Pizza.h"
 #include "MrCat.h"
 #include "Soup.h"
 #include "Pasta.h"
 #include "Order.h"
 #include <iostream>
+
+#include <thread>
 using namespace std;
 
+
+void testThread() {
+	while (true) {
+		int i = 5;
+		double a = 4;
+		while (2000000000) {
+			i += a*a;
+			a -= 1;
+		}
+		cout << ".";
+		this_thread::sleep_for(3s);
+	}
+}
 void main() 
 {
+	
+	thread t1(testThread); //як параметр конструктора передаємо вказівник на функцію 
+	//запустить на виконання паралельно з мейн
+	thread t2(testThread);
+	thread t3(testThread);
+	
+	while (true) {
+		int i = 5;
+		double a = 4;
+		while (i<2000000000) {
+			i += a*a;
+			a -= 1;
+		}
+		cout << "*";
+		this_thread::sleep_for(2s);
+	}
+	t1.join();
+	cout << "\n Thread 1 finished!" << endl;
+	t2.join();
+	t3.join();
+	//буде чекати, поки не закінчиться цей потік 
+	return;
 	MrCat cafe;
 	cafe.showMenu();
 
