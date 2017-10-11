1256e803c38b4c2607026937dcb68ba700a2a1e2
marshcherbak <maryshcherbak@gmail.com>
2017-10-11 13:13:28 +0300

    add dishes to order

--- Project1/Main.cpp
+++ Project1/Main.cpp
@@ -4,51 +4,89 @@
 #include "Pasta.h"
 #include "Order.h"
 #include <iostream>
-
+#include <mutex>
 #include <thread>
 using namespace std;
 
+std::mutex itemslock;
+vector<int> items = { 1,2,3,4,5,6,6 };
 
-void testThread() {
-	while (true) {
-		int i = 5;
-		double a = 4;
-		while (2000000000) {
-			i += a*a;
-			a -= 1;
-		}
-		cout << ".";
-		this_thread::sleep_for(3s);
+void addThread() {
+	int i = 0;
+	while (i < 50000000) {
+		itemslock.lock();
+		items.push_back(i*i);
+		itemslock.unlock();
+		//this_thread::sleep_for(0.00000001s);
+		i++;
 	}
 }
+void removeThread() {
+	int i = 0;
+	while (i < 50000000) {
+		itemslock.lock();
+		for (int k = 0; k < 100; k++) {
+			if (items.size() > 0)
+				items.pop_back();
+		}
+
+
+		//this_thread::sleep_for(0.00000001s);
+		itemslock.unlock();
+		i++;
+	}
+}
+
+void testsets() {
+
+	thread workers[] = { thread(addThread),  thread(addThread), thread(addThread),   thread(removeThread), thread(removeThread) }; //як параметр конструктора передаємо вказівник на функцію 
+																																   //запустить на виконання паралельно з мейн
+	int k = 0;
+	while (k < 500) {
+		cout << items.size() << endl;
+		this_thread::sleep_for(1s);
+		k++;
+	}
+	for (int j = 0; j < 3; j++)
+		workers[j].join();
+
+
+	cout << "\n All threads 1 finished!" << endl;
+
+	//буде чекати, поки не закінчиться цей потік 
+}
+//============================================================
+//============================================================
+
 void main() 
 {
-	
-	thread t1(testThread); //як параметр конструктора передаємо вказівник на функцію 
-	//запустить на виконання паралельно з мейн
-	thread t2(testThread);
-	thread t3(testThread);
-	
-	while (true) {
-		int i = 5;
-		double a = 4;
-		while (i<2000000000) {
-			i += a*a;
-			a -= 1;
-		}
-		cout << "*";
-		this_thread::sleep_for(2s);
-	}
-	t1.join();
-	cout << "\n Thread 1 finished!" << endl;
-	t2.join();
-	t3.join();
-	//буде чекати, поки не закінчиться цей потік 
-	return;
+	int tableNum = 3;
 	MrCat cafe;
 	cafe.showMenu();
 
-	Order newOrder(3);
+	Order newOrder(tableNum);
+
+	char c = -1;
+	while (c != '0') {
+		Dish *newDish = nullptr;
+		cin >> c;
+		switch (c) {
+		case '1':
+			newDish = new Pizza("QuadroFormagio");
+			break;
+		case '2':
+			newDish = new Soup("Miso");
+			break;
+		case '3':newDish = new Pasta("Passssta");
+			break;
+		default:
+			break;
+		}
+		if (newDish != nullptr) {
+			newOrder += newDish;
+			cout << newDish->getName() << " added to order.\n";
+		}
+	}
 	newOrder += new Pizza("QuattroFormagio");
 	newOrder += new Soup("Miso");
 
